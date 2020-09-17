89aa8df585114fedb72a5d04303e274816af8e08
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-09-16 21:47:39 -0300

    WIP - implementado service e repository para categoria

--- src/main/java/com/icarosouza/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/icarosouza/cursomc/resources/CategoriaResource.java
@@ -1,28 +1,37 @@
 package com.icarosouza.cursomc.resources;
 
 import com.icarosouza.cursomc.domain.Categoria;
+import com.icarosouza.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @GetMapping
-    public List<Categoria> listar() {
+    @Autowired
+    private CategoriaService service;
 
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
 
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add((cat2));
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
 
-        return lista;
     }
 
 }
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/icarosouza/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.icarosouza.cursomc.services;
+
+import com.icarosouza.cursomc.domain.Categoria;
+import com.icarosouza.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+
+    }
+
+}
