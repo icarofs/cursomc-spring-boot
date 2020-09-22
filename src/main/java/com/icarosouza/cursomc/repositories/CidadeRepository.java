9b4d723a464295ac6516dd3fcc391f247bf6a6e3
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-09-21 21:56:00 -0300

    WIP - Criado domain e repository de Cidade e Estado

---
+++ src/main/java/com/icarosouza/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,10 @@
+package com.icarosouza.cursomc.repositories;
+
+import com.icarosouza.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
