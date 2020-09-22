2316de870a131a6ce93087769ceee6e92ed60b6d
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-09-21 21:30:00 -0300

    WIP - Criação de exceções customizadas e ajustes no endpoint /categorias/{id}

--- src/main/java/com/icarosouza/cursomc/services/CategoriaService.java
+++ src/main/java/com/icarosouza/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.icarosouza.cursomc.services;
 
 import com.icarosouza.cursomc.domain.Categoria;
 import com.icarosouza.cursomc.repositories.CategoriaRepository;
+import com.icarosouza.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,8 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 
     }
 
