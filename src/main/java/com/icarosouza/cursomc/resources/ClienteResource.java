b934eff2edac5af3b269fe65515059440537e4db
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-09-24 22:39:49 -0300

    WIP - Criado resource e service de Cliente.

---
+++ src/main/java/com/icarosouza/cursomc/resources/ClienteResource.java
@@ -0,0 +1,37 @@
+package com.icarosouza.cursomc.resources;
+
+import com.icarosouza.cursomc.domain.Cliente;
+import com.icarosouza.cursomc.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+
+        Cliente obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
