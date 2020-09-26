6a801a762abe6c93a4bf4e7c949cde081236b996
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-09-26 14:07:12 -0300

    WIP - Mapeamento OneToOne (Pagamento e Pedido)

--- src/main/java/com/icarosouza/cursomc/domain/Pagamento.java
+++ src/main/java/com/icarosouza/cursomc/domain/Pagamento.java
@@ -3,12 +3,23 @@ package com.icarosouza.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Pagamento implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     private Integer id;
     private Integer estado;
 
+    @OneToOne
+    @JoinColumn(name="pedido_id")
+    @MapsId
     private Pedido pedido;
 
     public Pagamento() {
